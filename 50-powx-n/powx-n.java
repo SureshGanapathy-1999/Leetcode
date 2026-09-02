9b804ca40d61a1c12b5d611326b2d17429305049
Suresh Ganapathy <sureshganapathy5349@gmail.com>
2026-09-02 23:06:36 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (22.14%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public double myPow(double x, int n) {
+
+        long power = n;
+
+        if (power < 0) {
+            x = 1 / x;
+            power = -power;
+        }
+
+        return power(x, power);
+    }
+
+    public double power(double x, long n) {
+
+        if (n == 0) {
+            return 1;
+        }
+
+        double half = power(x, n / 2);
+
+        if (n % 2 == 0) {
+            return half * half;
+        }
+
+        return x * half * half;
+    }
+}
